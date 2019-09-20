f64bc818ff52560e3690be52f39c3087f7269872
DASSE Anthony <anthony.dasse@soprasteria.com>
2019-09-20 21:17:53 +0200

    add total distance per week / add features on readme

--- webapp/src/components/TrainingPlan/WeekSessions.vue
+++ webapp/src/components/TrainingPlan/WeekSessions.vue
@@ -1,9 +1,10 @@
 <template>
 <!-- v-bind:title="post.title + ' par ' + post.author.name"  -->
-   <b-card no-body v-bind:header="'Week ' + numWeek" variant="danger">
+   <b-card no-body v-bind:header="'Week ' + numWeek + ' (Total distance: '+ distance(sessions)+' )'" variant="danger">
     <b-list-group flush>
-      <b-table striped hover :items="items"></b-table>
+      <b-table striped hover :fields="fields" :items="sessions"></b-table>
     </b-list-group>
+     <b-button  type="submit">Add session</b-button>
   </b-card>
 </template>
 
@@ -11,19 +12,40 @@
 export default {
     name : 'WeekSessions',
     props: {
-        numWeek: Number
+        numWeek: Number,
+        sessions: Array
     },
      data() {
       return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+       fields: [
+                    {
+            // A regular column with custom formatter
+            key: 'date',
+            formatter: value => {
+                let tmp = new Date (value);
+              return tmp.getDay();
+            }
+          },
+           'name',
+            'type',
+            'distance',
+            'time',
+            'heightDifference',
+            'circuit',
+            'details'
+
+       ]       
       }
+    },
+    methods: {
+      distance: (sessions) => {         
+         return sessions.map(session => session.distance).reduce((a,b) => {
+           return a + b
+           }, 0);
+       }
     }
 }
+
 </script>
 
 <style>
